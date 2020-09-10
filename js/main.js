6f84f90d37e199d4b260c8c0009fee0629063bf5
Dmitro Krivovichev <angeldemon90@mail.ru>
2020-09-10 19:35:20 +0300

    Slider and Search added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-$(function(){
-
+$(function () {
+  $('.slider').slick({
+    prevArrow: ` <button  class="slick-arrow slick-prev">
+        <img src="images/arrow-prev.svg" alt="prev arrow" />
+      </button>`,
+    nextArrow: `
+      <button class="slick-arrow slick-next">
+        <img src="images/arrow-next.svg" alt="next arrow" />
+      </button>`,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 });
